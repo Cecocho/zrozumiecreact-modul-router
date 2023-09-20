e8f7149bc8229a0f8b0d66c4313d87ec81b94733
Kacper Sokołowski <kontakt@kaapa.pl>
2023-09-20 15:57:31 +0200

    Lekcja 6: koniec

--- front-end/src/components/note/Note.jsx
+++ front-end/src/components/note/Note.jsx
@@ -1,12 +1,14 @@
 import RemoveIcon from "../../assets/remove.svg";
 import styles from "./Note.module.css";
 import { TopBar } from "../top-bar/TopBar";
+import { useLoaderData } from "react-router-dom";
 
 const NoteEditor = ({ children }) => (
     <div className={styles["note-editor"]}>{children}</div>
 );
 
 const Note = () => {
+    const note = useLoaderData();
     return (
         <div className={styles.container}>
             <TopBar>
@@ -16,8 +18,8 @@ const Note = () => {
             </TopBar>
 
             <NoteEditor>
-                <input type="text" />
-                <textarea />
+                <input type="text" defaultValue={note.title} />
+                <textarea defaultValue={note.body} />
             </NoteEditor>
         </div>
     );
