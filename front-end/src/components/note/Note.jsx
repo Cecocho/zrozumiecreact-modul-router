5149959d5e204b435b969844967051be5634337e
Kacper Sokołowski <kontakt@kaapa.pl>
2023-11-22 10:23:09 +0100

    Rozwiązanie zadania z PATCH i debounce

--- front-end/src/components/note/Note.jsx
+++ front-end/src/components/note/Note.jsx
@@ -2,6 +2,8 @@ import RemoveIcon from "../../assets/remove.svg";
 import styles from "./Note.module.css";
 import { TopBar } from "../top-bar/TopBar";
 import { useLoaderData, Form, useSubmit, redirect } from "react-router-dom";
+import { useCallback } from "react";
+import { debounce } from "../../utils/debounce";
 
 const NoteEditor = ({ children }) => (
     <div className={styles["note-editor"]}>{children}</div>
@@ -37,6 +39,12 @@ const Note = () => {
     const note = useLoaderData();
     const submit = useSubmit();
 
+    const onChangeCallback = useCallback(
+        debounce((event) => {
+            submit(event.currentTarget, { method: "PATCH" });
+        }, 300)
+    );
+
     return (
         <div className={styles.container}>
             <TopBar>
@@ -46,13 +54,8 @@ const Note = () => {
                     </button>
                 </Form>
             </TopBar>
-            <Form
-                method="PATCH"
-                onChange={(event) => {
-                    submit(event.currentTarget);
-                }}
-            >
-                <NoteEdito key={note.id}r>
+            <Form method="PATCH" onChange={onChangeCallback}>
+                <NoteEditor key={note.id} r>
                     <input type="text" defaultValue={note.title} name="title" />
                     <textarea defaultValue={note.body} name="body" />
                 </NoteEditor>
